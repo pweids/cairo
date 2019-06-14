6311f1ccc4e96b40128497dc1df5a8167221471f
pweids <paul.weidinger@gmail.com>
2019-06-14 14:21:34 -0400

    changing from 2 to 4 spaces for readability

--- src/cairo.py
+++ src/cairo.py
@@ -14,233 +14,256 @@ PKL_FILE = '.cairo.pkl'
 ignored = [IGNORE_FILE, PKL_FILE]
 
 
-## Data Structures
+# Data Structures
 
 Version = namedtuple("Version", "verid time")
 
+
 @dataclass
 class Mod:
-  version: Version # the GUID of the version
-  field:   str     # the field from the TimeFile
-  value:   str     # the value at this version num
+    version: Version  # the GUID of the version
+    field:   str     # the field from the TimeFile
+    value:   str     # the value at this version num
 
 
 class FileType(Enum):
-  Dir   = 1
-  Text  = 2
-  #Image = 3
+    Dir = 1
+    Text = 2
+    #Image = 3
 
 
 @dataclass
 class FileTree:
-  filepath: Path           # this file's name. Not using paths here
-  data:     str            # the textual data in this file if Text file
-  filetype: FileType
-  mods:     List[Mod] = field(default_factory=list) # a list of modifications done to this
-  init:     datetime = datetime.now() # when this file was initialized
-  ID:       UUID = field(init=False)
-  children: List[UUID] = field(default_factory=list, init=False)
+    filepath: Path           # this file's name. Not using paths here
+    data:     str            # the textual data in this file if Text file
+    filetype: FileType
+    # a list of modifications done to this
+    mods:     List[Mod] = field(default_factory=list)
+    init:     datetime = datetime.now()  # when this file was initialized
+    ID:       UUID = field(init=False)
+    children: List[UUID] = field(default_factory=list, init=False)
 
-  def __post_init__(self):
-    self.ID = uuid1()
-    File_Index[self.ID] = self
+    def __post_init__(self):
+        self.ID = uuid1()
+        File_Index[self.ID] = self
 
-    if isinstance(self.filepath, str):
-      self.filepath = Path(self.filepath)
+        if isinstance(self.filepath, str):
+            self.filepath = Path(self.filepath)
 
-   
-    if self.filetype == FileType.Dir:
-      for f in self.filepath.iterdir():
-        ft = _create_file_tree(f)
-        if ft: self.children.append(ft.ID)
-  
-  def __str__(self):
-    return f"FileTree(filepath='{self.filepath}', " \
-      "filetype='{self.filetype}', id='{self.ID}')"
+        if self.filetype == FileType.Dir:
+            for f in self.filepath.iterdir():
+                ft = _create_file_tree(f)
+                if ft:
+                    self.children.append(ft.ID)
+
+    def __str__(self):
+        return f"FileTree(filepath='{self.filepath}', " \
+            "filetype='{self.filetype}', id='{self.ID}')"
+
 
 File_Index = {}
 
-## Query
+
+# Query
 
 def ft_at_time(root: FileTree, dt: datetime) -> FileTree:
-  """ Return this FileTree's state at the specified time """
-  pass
+    """ Return this FileTree's state at the specified time """
+    pass
 
 
 def get_versions(root: FileTree) -> List[Version]:
-  """ Returns all of the versions in the FileTree """
-  def go(ft, vs):
-    vs = vs.union(set([m.version for m in ft.mods]))
-    for c in ft.children:
-      vs = go(File_Index[c], vs)
-    return vs
-  vs = go(root, set())
-  return sorted(vs, key=lambda v: v.time, reverse=True)
+    """ Returns all of the versions in the FileTree """
+    def go(ft, vs):
+        vs = vs.union(set([m.version for m in ft.mods]))
+        for c in ft.children:
+            vs = go(File_Index[c], vs)
+        return vs
+    vs = go(root, set())
+    return sorted(vs, key=lambda v: v.time, reverse=True)
 
 
 def get_versions_of_file(root: FileTree, ID: str) -> List[Version]:
-  """ Return all the versions for a specific file """
-  f = _find_file(root, ID)
-  return [m.version for m in f.mods] if f else None
+    """ Return all the versions for a specific file """
+    f = _find_file(root, ID)
+    return [m.version for m in f.mods] if f else None
 
 
 def last_changed(root: FileTree, ID: str) -> datetime:
-  f = _find_file(root, ID)
-  if f:
-    return f.mods[-1].version.time if f.mods else f.init
-  else:
-    return None
+    f = _find_file(root, ID)
+    if f:
+        return f.mods[-1].version.time if f.mods else f.init
+    else:
+        return None
 
 
 def last_save_time(root: FileTree) -> datetime:
-  gv = get_versions(root)
-  return gv[-1] if gv else root.init
+    gv = get_versions(root)
+    return gv[-1] if gv else root.init
 
 
 def find_file(ft: FileTree, name: str) -> FileTree:
-  """ Returns the file ID of the named file in root. Finds
-  the most shallow instance
-  """
-  if ft.filepath.name == name: return ft
-  else:
-    for c in ft.children:
-      f = find_file(File_Index[c], name)
-      if f: return f
-    return None
+    """ Returns the file ID of the named file in root. Finds
+    the most shallow instance
+    """
+    if ft.filepath.name == name:
+        return ft
+    else:
+        for c in ft.children:
+            f = find_file(File_Index[c], name)
+            if f:
+                return f
+        return None
 
 
 def find_file_path(ft: FileTree, fp: Path) -> FileTree:
-  if ft.filepath == fp: return ft
-  else:
-    for c in ft.children:
-      f = find_file_path(File_Index[c], fp)
-      if f: return f
-    return None
+    if ft.filepath == fp:
+        return ft
+    else:
+        for c in ft.children:
+            f = find_file_path(File_Index[c], fp)
+            if f:
+                return f
+        return None
 
 
 def resolve(ft: FileTree) -> dict:
-  """ Return the contents of the file after all modifications """
-  data = {}
-  for m in ft.mods:
-    data[m.field] = m.value
-  return data
+    """ Return the contents of the file after all modifications """
+    data = {}
+    for m in ft.mods:
+        data[m.field] = m.value
+    return data
 
 
-## Setup
+# Setup
 
 def init(fp: Path = Path()) -> FileTree:
-  """ Initialize Cairo at the given path (or the current working directoy)
-  if not supplied. Looks for a History, and if not there, creates one.
-  """
-  tree_file = fp/PKL_FILE
-  global ignored # doing this to cache the files
-  ignored = _ignored_files(fp/IGNORE_FILE)
+    """ Initialize Cairo at the given path (or the current working directoy)
+    if not supplied. Looks for a History, and if not there, creates one.
+    """
+    tree_file = fp/PKL_FILE
+    global ignored  # doing this to cache the files
+    ignored = _ignored_files(fp/IGNORE_FILE)
 
-  try:
-    with open(tree_file, 'rb') as tf:
-      return pickle.load(tf)
-  except:
-    ft = _create_file_tree(fp)
-    save(ft)
-    return ft
+    try:
+        with open(tree_file, 'rb') as tf:
+            return pickle.load(tf)
+    except:
+        ft = _create_file_tree(fp)
+        save(ft)
+        return ft
 
 
-## File Changes
+# File Changes
 
 def changed_files(root: FileTree) -> List[Path]:
-  """ List all files changed since the most recent version """
-  time = last_save_time(root)
-  changed = []
-  
-  def is_new(fp):
-    if fp.name in ignored: return False
-    EPS = 40_000 # microseconds
-    mtime = datetime.fromtimestamp(fp.stat().st_mtime)
-    if (mtime - time).microseconds > EPS:
-      print(fp.name, mtime-time)
-      return True
-    else: return False
+    """ List all files changed since the most recent version """
+    time = last_save_time(root)
+    changed = []
 
-  def go(fp):
-    for f in fp.iterdir():
-      if is_new(f):
-        changed.append(f)
-      if (f.is_dir()):
-        go(f)
+    def is_new(fp):
+        if fp.name in ignored:
+            return False
+        EPS = 40_000  # microseconds
+        mtime = datetime.fromtimestamp(fp.stat().st_mtime)
+        if (mtime - time).microseconds > EPS:
+            print(fp.name, mtime-time)
+            return True
+        else:
+            return False
 
-  go(root.filepath)
-  return changed
+    def go(fp):
+        for f in fp.iterdir():
+            if is_new(f):
+                changed.append(f)
+            if (f.is_dir()):
+                go(f)
+
+    go(root.filepath)
+    return changed
 
 
 def commit(root: FileTree) -> None:
-  """ Commit all modifcations in the local directory to the data structure """
-  pass
+    """ Commit all modifcations in the local directory to the data structure """
+    pass
 
 
 def save(root: FileTree) -> None:
-  tree_file = root.filepath/PKL_FILE
-  with open(tree_file, 'wb') as tf:
-    pickle.dump(root, tf)
+    tree_file = root.filepath/PKL_FILE
+    with open(tree_file, 'wb') as tf:
+        pickle.dump(root, tf)
 
 
-## Commands
+# Commands
 
 def rm_file(root: FileTree, fp: Path) -> FileTree:
-  """ Remove the file from the FileTree """
-  pass
+    """ Remove the file from the FileTree """
+    pass
 
 
 def mv_file(root: FileTree, fp: Path, parent: Path) -> FileTree:
-  """ Move "file" to "parent" directory """
-  assert parent.is_dir()
-  ft = find_file_path(root, fp)
-  p1 = find_file_path(root, fp.parent)
-  p2 = find_file_path(root, parent)
+    """ Move "file" to "parent" directory """
+    assert parent.is_dir()
+    ft = find_file_path(root, fp)
+    p1 = find_file_path(root, fp.parent)
+    p2 = find_file_path(root, parent)
 
-  if not all([ft, p1, p2]): return
-  p1c = copy(p1.children).remove(ft.ID)
-  p2c = copy(p2.children).append(ft.ID)
-  _add_new_mod(p1, 'children', p1c)
-  _add_new_mod(p2, 'children', p2c)
+    if not all([ft, p1, p2]):
+        return
 
-  newfp = parent/fp.name
-  fp.rename(newfp)
-  _add_new_mod(ft, 'filepath', newfp)
+    v = _mk_ver()
 
-## helpers
+    p1c = copy(p1.children).remove(ft.ID)
+    p2c = copy(p2.children).append(ft.ID)
+    _add_new_mod(p1, 'children', p1c, v)
+    _add_new_mod(p2, 'children', p2c, v)
+
+    newfp = parent/fp.name
+    fp.rename(newfp)
+    _add_new_mod(ft, 'filepath', newfp, v)
+
+# helpers
+
 
 def _ignored_files(fp: Path) -> List[str]:
-  """ Return a list of all file names listed in the fp ignore file """
-  ns = ignored
-  if fp.exists() and fp.is_file():
-    with open(fp, 'r') as f:
-      ns += f.readlines()
-  return ns
+    """ Return a list of all file names listed in the fp ignore file """
+    ns = ignored
+    if fp.exists() and fp.is_file():
+        with open(fp, 'r') as f:
+            ns += f.readlines()
+    return ns
 
 
 def _create_file_tree(fp: Path) -> FileTree:
-  if fp.name in ignored: return None
-  if fp.is_dir():
-    ft = FileType.Dir
-    d  = None
-  else:
-    ft = FileType.Text
-    try:
-      with open(fp, 'r') as f:
-        d = f.read()
-    except:
-      return None
-  return FileTree(fp, d, ft)
-  
+    if fp.name in ignored:
+        return None
+    if fp.is_dir():
+        ft = FileType.Dir
+        d = None
+    else:
+        ft = FileType.Text
+        try:
+            with open(fp, 'r') as f:
+                d = f.read()
+        except:
+            return None
+    return FileTree(fp, d, ft)
+
 
 def _find_file(root: FileTree, ID: UUID) -> FileTree:
-  if root.ID == ID: return root
-  else:
-    for c in root.children:
-      f = _find_file(c, ID)
-      if f: return f
-    return None
+    if root.ID == ID:
+        return root
+    else:
+        for c in root.children:
+            f = _find_file(c, ID)
+            if f:
+                return f
+        return None
 
-def _add_new_mod(ft: FileTree, key, val) -> None:
-  v = Version(uuid1(), datetime.now())
-  ft.mods.append(Mod(v, key, val))
+
+def _add_new_mod(ft: FileTree, key, val, version=None) -> None:
+    v = version or _mk_ver()
+    ft.mods.append(Mod(v, key, val))
+
+
+def _mk_ver() -> Version:
+    return Version(uuid1(), datetime.now())
--- test/test_cairo.py
+++ test/test_cairo.py
@@ -1,3 +1,4 @@
+from cairo import *
 import tempfile
 import os
 import sys
@@ -10,151 +11,153 @@ import time
 
 sys.path.append('./src')
 
-from cairo import *
 
 @pytest.fixture
 def cleandir():
-  newpath = Path(tempfile.mkdtemp())
-  os.chdir(newpath)
-  (newpath/'test_dir').mkdir()
-  (newpath/'test_dir'/'test.txt').touch()
-  (newpath/'test_dir'/'empty_dir').mkdir()
-  (newpath/'test_dir'/'sub_dir').mkdir()
-  (newpath/'test_dir'/'sub_dir'/'test2.txt').touch()
-  (newpath/'ignore_me.txt').touch()
+    newpath = Path(tempfile.mkdtemp())
+    os.chdir(newpath)
+    (newpath/'test_dir').mkdir()
+    (newpath/'test_dir'/'test.txt').touch()
+    (newpath/'test_dir'/'empty_dir').mkdir()
+    (newpath/'test_dir'/'sub_dir').mkdir()
+    (newpath/'test_dir'/'sub_dir'/'test2.txt').touch()
+    (newpath/'ignore_me.txt').touch()
 
-  ignore = newpath/IGNORE_FILE
-  with open(ignore, 'w') as f:
-    f.write('ignore_me.txt')
+    ignore = newpath/IGNORE_FILE
+    with open(ignore, 'w') as f:
+        f.write('ignore_me.txt')
 
-  with open((newpath/'test_dir'/'test.txt'), 'w') as f:
-    f.write('test1')
-  yield
-  rmtree(newpath.absolute())
+    with open((newpath/'test_dir'/'test.txt'), 'w') as f:
+        f.write('test1')
+    yield
+    rmtree(newpath.absolute())
 
 
 def test_init_empty_dir(cleandir):
-  curp = Path()
-  assert not (curp/PKL_FILE).exists()
-  root = init(Path())
-  assert isinstance(root, FileTree)
-  assert  (curp/PKL_FILE).exists()
+    curp = Path()
+    assert not (curp/PKL_FILE).exists()
+    root = init(Path())
+    assert isinstance(root, FileTree)
+    assert (curp/PKL_FILE).exists()
 
 
 def test_find_files(cleandir):
-  root = init()
-  ft = find_file(root, 'test2.txt')
-  assert ft is not None
-  ft2 = find_file_path(root, Path('./test_dir/test.txt'))
-  assert ft2 is not None
+    root = init()
+    ft = find_file(root, 'test2.txt')
+    assert ft is not None
+    ft2 = find_file_path(root, Path('./test_dir/test.txt'))
+    assert ft2 is not None
 
 
 def test_cannot_find_ignored_file(cleandir):
-  root = init()
-  ft = find_file(root, 'ignore_me.txt')
-  assert ft is None
+    root = init()
+    ft = find_file(root, 'ignore_me.txt')
+    assert ft is None
 
 
 def test_cannot_find_file_doesnt_exist(cleandir):
-  root = init()
-  ft = find_file(root, 'not_there')
-  assert ft is None
+    root = init()
+    ft = find_file(root, 'not_there')
+    assert ft is None
 
 
 def test_recent_versions(cleandir):
-  root = init()
-  ft = find_file(root, 'test.txt')
+    root = init()
+    ft = find_file(root, 'test.txt')
 
-  v1 = get_versions(root)
-  assert not v1
+    v1 = get_versions(root)
+    assert not v1
 
-  v = Version(uuid1(), datetime.now())
-  mod = Mod(v, None, None)
-  ft.mods.append(mod)
+    v = Version(uuid1(), datetime.now())
+    mod = Mod(v, None, None)
+    ft.mods.append(mod)
 
-  v2 = get_versions(root)
-  assert v2
-  assert v2[0].time > root.init
+    v2 = get_versions(root)
+    assert v2
+    assert v2[0].time > root.init
 
 
 def test_resolve_version(cleandir):
-  root = init()
-  ft = find_file(root, 'test.txt')
+    root = init()
+    ft = find_file(root, 'test.txt')
 
-  v = Version(uuid1(), datetime.now())
-  mod = Mod(v, 'data', 'test2')
-  ft.mods.append(mod)
+    v = Version(uuid1(), datetime.now())
+    mod = Mod(v, 'data', 'test2')
+    ft.mods.append(mod)
 
-  assert ft.data == 'test1'
-  fdata = resolve(ft)
-  assert fdata['data'] == 'test2'
+    assert ft.data == 'test1'
+    fdata = resolve(ft)
+    assert fdata['data'] == 'test2'
 
-  
+
 def test_mv_file(cleandir):
-  root = init()
-  f = Path()
-  f = f/'test_dir'/'test.txt'
-  p = Path()/'test_dir'/'sub_dir'
-  newfp = p/'test.txt'
+    root = init()
+    f = Path()
+    f = f/'test_dir'/'test.txt'
+    p = Path()/'test_dir'/'sub_dir'
+    newfp = p/'test.txt'
 
-  mv_file(root, f, p)
+    mv_file(root, f, p)
 
-  print(f)
-  assert not f.exists()
-  assert newfp.exists()
-  ft = find_file(root, 'test.txt')
-  assert resolve(ft)['filepath'] == newfp
+    print(f)
+    assert not f.exists()
+    assert newfp.exists()
+    ft = find_file(root, 'test.txt')
+    assert resolve(ft)['filepath'] == newfp
+
+    assert len(get_versions(root)) == 1
 
 
 def test_changed(cleandir):
-  root = init()
-  cf = changed_files(root)
-  assert len(cf) == 0
+    root = init()
+    cf = changed_files(root)
+    assert len(cf) == 0
 
-  p = Path()/'test_dir'/'test.txt'
-  time.sleep(.02)
-  with open(p, 'w') as f:
-    f.write('change')
-  
-  cf = changed_files(root)
-  assert len(cf) == 1
+    p = Path()/'test_dir'/'test.txt'
+    time.sleep(.02)
+    with open(p, 'w') as f:
+        f.write('change')
+
+    cf = changed_files(root)
+    assert len(cf) == 1
 
 
 def test_new_file(cleandir):
-  assert not list(Path().glob('.cairo.pkl'))
-  root = init()
+    assert not list(Path().glob('.cairo.pkl'))
+    root = init()
 
-  p = Path()/'new_file.txt'
-  p.touch()
+    p = Path()/'new_file.txt'
+    p.touch()
 
-  cf = changed_files(root)
-  assert p in cf
+    cf = changed_files(root)
+    assert p in cf
 
 
 def dont_test_remove_file(cleandir):
-  p4 = Path()/'test_dir'/'sub_dir'/'test2.txt'
-  rm_file(root, fp)
+    p4 = Path()/'test_dir'/'sub_dir'/'test2.txt'
+    rm_file(root, fp)
+
 
 def dont_test_commit(cleandir):
-  root = init()
+    root = init()
 
-  # new file
-  p = Path()/'new_file.txt'
-  p.touch()
+    # new file
+    p = Path()/'new_file.txt'
+    p.touch()
 
-  # changed file
-  p2 = Path()/'test_dir'/'test.txt'
-  with open(p2, 'w') as f:
-    f.write('change')
+    # changed file
+    p2 = Path()/'test_dir'/'test.txt'
+    with open(p2, 'w') as f:
+        f.write('change')
 
-  # moved file
-  f = Path()
-  f = f/'test_dir'/'sub_dir'/'test.txt'
-  p3 = Path()/'test_dir'/'empty_dir'
-  newfp = p3/'test.txt'
+    # moved file
+    f = Path()
+    f = f/'test_dir'/'sub_dir'/'test.txt'
+    p3 = Path()/'test_dir'/'empty_dir'
+    newfp = p3/'test.txt'
 
-  mv_file(root, f, p3)
+    mv_file(root, f, p3)
 
-  # removed file
-  p4 = Path()/'test_dir'/'sub_dir'/'test2.txt'
-  rm_file(root, fp)
+    # removed file
+    p4 = Path()/'test_dir'/'sub_dir'/'test2.txt'
+    rm_file(root, fp)
